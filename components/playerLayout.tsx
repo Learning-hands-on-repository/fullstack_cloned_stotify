3aa30bf82a2040cd51a8a168b583bb1e33f2f07c
Nara <narawit.tub@outlook.co.th>
2022-04-10 13:06:13 +0700

    :lipstick: (Layout) ui: Playlist container setup

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,7 +1,26 @@
 import { Box } from "@chakra-ui/layout";
 
 const PlayerLayout = ({ children }) => {
-  return <Box>Player Layout{children}</Box>;
+  return (
+    // 100vw, 100vh preventing the scrolling of the whole page, we only need the scrolling of individual element
+    <Box width="100vw" height="100vh">
+      <Box
+        position="absolute"
+        top="0"
+        width="250px"
+        left="0"
+        backgroundColor="red.200"
+      >
+        Sidebar
+      </Box>
+      <Box marginLeft="250px" backgroundColor="red.300">
+        {children}
+      </Box>
+      <Box position="absolute" bottom="0" left="0" backgroundColor="red.500">
+        Player
+      </Box>
+    </Box>
+  );
 };
 
 export default PlayerLayout;
